8dc9efb1e31e7000c122a952ae46546c40ecfb85
Agusti Baubi <abaubi@gmail.com>
2019-10-19 12:16:00 +0200

    version 1.3

--- build/core/WMath.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\abaub\Documents\Arduino\generated_examples\BlinkSerial\build\core\WMath.cpp.o: \
- C:\Users\abaub\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.8.3\cores\arduino\WMath.cpp \
- C:\Users\abaub\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.8.3\cores\arduino\WMath.h
--- build/core/itoa.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\abaub\Documents\Arduino\generated_examples\BlinkSerial\build\core\itoa.c.o: \
- C:\Users\abaub\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.8.3\cores\arduino\itoa.c \
- C:\Users\abaub\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.8.3\cores\arduino\itoa.h
